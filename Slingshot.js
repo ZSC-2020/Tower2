d7942702ab1d4c76732774e87c46dcb6bed1e8ed
ZSC-2020 <72572076+ZSC-2020@users.noreply.github.com>
2021-01-21 11:51:38 -0600

    Add files via upload

---
+++ Slingshot.js
@@ -0,0 +1,37 @@
+class SlingShot{
+constructor(bodyA,pointB){
+ var option={
+  bodyA:bodyA,
+  pointB:pointB,
+  stiffness:0.02,
+  length:30,
+ }
+ 
+ this.pointB=pointB;
+ this.sling=Constraint.create(option);
+ World.add(world,this.sling);
+}
+fly(){
+this.sling.bodyA=null
+
+}
+
+display(){
+    
+if(this.sling.bodyA){
+
+   
+var pointA=this.sling.bodyA.position
+var pointB=this.pointB
+
+push()
+strokeWeight(10);
+stroke(43,200,8)
+line(pointA.x,pointA.y,pointB.x,pointB.y)
+
+
+pop()
+}
+}
+
+}
